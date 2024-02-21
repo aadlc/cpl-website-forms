dedd869d6d14a5ba074870beb4c31266f1d7174d
Alfonzo <alfonzo.angulodelacruz@calgarylibrary.ca>
2024-02-20 22:31:49 -0700

    12345

--- src/app/forms/art-exhibit-form/page.jsx
+++ src/app/forms/art-exhibit-form/page.jsx
@@ -1,16 +1,10 @@
 import ArtExhibitForm from '@components/ArtExhibitForm'
-import { getToken, getListOfBranches } from '@utils/actions'
 
 export default async function  ArtExhibitPage() {
-  let locations
-  const token = await getToken()
-  if (token){
-    locations = await getListOfBranches(token)
-  }
 
   return (
     <>
-      <ArtExhibitForm locations={locations} />
+      <ArtExhibitForm  />
     </>
   )
 }
